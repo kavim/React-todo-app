cab286ed4340cb05dc770cca8b8a724a24039396
Kavim <kavim@pop-os.localdomain>
2020-11-06 00:36:53 -0300

    Init

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Form = (props) => {
+
+    //Aqui eu posso escrever javascript e funções... e blablablas
+
+    const inputTextHandler = (e) => {
+        // console.log(e);
+        console.log(e.target.value);
+
+        props.setInputText(e.target.value);
+    }
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        props.setTodos([
+            ...props.todos, 
+            {text: props.inputText, completed: false, id: Math.random()*100}
+        ]);
+
+        props.setInputText("");
+
+    }
+
+    return (
+        <form>
+            <input value={props.inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+            <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+            <select name="todos" className="filter-todo">
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">Uncompleted</option>
+            </select>
+            </div>
+        </form>
+    );
+}
+
+export default Form;
